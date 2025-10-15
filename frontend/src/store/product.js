4453626eaaf17d4425fe880534644ad3f39691ee
np-shishir <shishir.nepal404@gmail.com>
2025-10-15 19:21:19 +0545

    api change

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -7,7 +7,7 @@ export const useProductStore = create((set) => ({
     if (!newProduct.name || !newProduct.image || !newProduct.price) {
       return { success: false, message: "Please fill in all fields." };
     }
-    const res = await fetch("http://localhost:5000/api/products", {
+    const res = await fetch("https://mern-crud-yk9x.onrender.com/api/products", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -19,12 +19,12 @@ export const useProductStore = create((set) => ({
     return { success: true, message: "Product created successfully" };
   },
   fetchProducts: async () => {
-    const res = await fetch("http://localhost:5000/api/products");
+    const res = await fetch("https://mern-crud-yk9x.onrender.com/api/products");
     const data = await res.json();
     set({ products: data.data });
   },
   deleteProduct: async (pid) => {
-    const res = await fetch(`http://localhost:5000/api/products/${pid}`, {
+    const res = await fetch(`https://mern-crud-yk9x.onrender.com/api/products/${pid}`, {
       method: "DELETE",
     });
     const data = await res.json();
@@ -37,7 +37,7 @@ export const useProductStore = create((set) => ({
     return { success: true, message: data.message };
   },
   updateProduct: async (pid, updatedProduct) => {
-    const res = await fetch(`http://localhost:5000/api/products/${pid}`, {
+    const res = await fetch(`https://mern-crud-yk9x.onrender.com/api/products/${pid}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
